6f3d26c613cb67312f4347e95549fa05d58509a6
Hasan Eren Akgöz <eren34akgoz@gmail.com>
2021-11-10 22:59:46 +0300

    Add project files.

---
+++ ExcelDataToJsonFormat/JsonClass.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelDataToJsonFormat
+{
+   public class JsonClass
+    {
+        [JsonProperty("label")]
+        public string label { get; set; }
+
+        [JsonProperty("tr-TR")]
+        public string TrTR { get; set; }
+
+        [JsonProperty("en-US")]
+        public string EnUS { get; set; }
+
+        [JsonProperty("fr-FR")]
+        public string FrFR { get; set; }
+
+        [JsonProperty("it-IT")]
+        public string ItIT { get; set; }
+
+        [JsonProperty("bg-BG")]
+        public string BgBG { get; set; }
+
+        [JsonProperty("ro-RO")]
+        public string RoRO { get; set; }
+
+        [JsonProperty("ru-RU")]
+        public string RuRU { get; set; }
+    }
+}
---
+++ ExcelDataToJsonFormat/Program.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.IO;
+
+namespace ExcelDataToJsonFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String filePath = @"D:\demo\demo.xlsx";
+            string dosya_yolu = @"D:\metinbelgesi.txt";
+
+            try
+            {
+                IWorkbook workbook = null;
+                FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+                FileStream fs = new FileStream(dosya_yolu, FileMode.OpenOrCreate, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(fs);
+
+                if (filePath.IndexOf(".xlsx") > 0)
+                {
+                    workbook = new XSSFWorkbook(fileStream);
+                }
+                else if (filePath.IndexOf(".xls") > 0)
+
+                    workbook = new HSSFWorkbook(fileStream);
+                Console.WriteLine(filePath.IndexOf(".xlsx"));
+                Console.WriteLine(filePath.IndexOf(".xlsx"));
+                ISheet sheet = workbook.GetSheetAt(0);
+                if (sheet != null)
+                {
+                    int rowCount = sheet.LastRowNum;
+                    for (int i = 0; i < rowCount; i++)
+                    {
+                        IRow curRow = sheet.GetRow(i);
+                        var cellValueTitle = curRow.GetCell(0).StringCellValue.Trim();
+                        var cellValueTr = curRow.GetCell(1).StringCellValue.Trim();
+                        var cellValueEn = curRow.GetCell(2).StringCellValue.Trim();
+                        var cellValueFr = curRow.GetCell(3).StringCellValue.Trim();
+                        var cellValueIt = curRow.GetCell(4).StringCellValue.Trim();
+                        var cellValueBg = curRow.GetCell(5).StringCellValue.Trim();
+                        var cellValueRo = curRow.GetCell(6).StringCellValue.Trim();
+                        var cellValueRu = curRow.GetCell(7).StringCellValue.Trim();
+                        JsonClass jsonClass = new JsonClass()
+                        {
+                            label = cellValueTitle,
+                            TrTR = cellValueTr,
+                            EnUS = cellValueEn,
+                            FrFR = cellValueFr,
+                            ItIT = cellValueIt,
+                            BgBG = cellValueBg,
+                            RoRO = cellValueRo,
+                            RuRU = cellValueRu,
+
+                        };
+                        string strResultJson = JsonConvert.SerializeObject(jsonClass);
+                        File.WriteAllText(@"jsonClass.json", strResultJson);
+                        sw.WriteLine(jsonClass.label + ": {\n"
+                            + "\"tr-Tr\":" + "\"" + jsonClass.TrTR 
+                            + "\",\n\"en-Us\":"+"\""+jsonClass.EnUS+ "\"," +
+                            "\n\"fr-FR\":"+"\""+jsonClass.FrFR+ "\"," +
+                            "\n\"it-IT\":" + "\"" + jsonClass.ItIT + "\"," +
+                            "\n\"bg-BG\":" + "\"" + jsonClass.BgBG + "\",\n\"ro-RO\":" + "\"" + jsonClass.RoRO+ "\"," +
+                            "\n\"ru-RU\":" + "\"" + jsonClass.RuRU + "\"\n},\n\n");
+                            
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+    }
+}
+
